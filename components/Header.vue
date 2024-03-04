e276c5ef7470d030191c3a42b32dca8afdcac26b
aarizona <ar.ariz.arizona@gmail.com>
2024-03-04 11:40:48 +0300

    save def data to local storage

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-
+const todos = useTodoStore()
 </script>
+
 <template>
-    <a-row class="header" align="middle">
+    <a-row class="header" align="middle" justify="space-between">
         <a-col span="4">
             <NuxtLink to="/">
                 <Icon name="ri:todo-fill" class="header-logo" />
             </NuxtLink>
+        </a-col>
+        <a-col>
+            <a-button @click="todos.clearLocalStorage">Очистить localstorage</a-button>
         </a-col>
     </a-row>
 </template>
--- stores/todos.ts
+++ stores/todos.ts
@@ -26,27 +26,47 @@ export const useTodoStore = defineStore('todoStore', {
         error: null
     }),
     actions: {
+        clearLocalStorage(){
+            localStorage.removeItem('projects')
+            localStorage.removeItem('todos')
+            this.fetch()
+        },
+        local(type: 'projects' | 'todos', error = false) {
+            let localResponse = null
+            try {
+                const lp = localStorage.getItem(type)
+                let localData = null
+                if (lp) localData = JSON.parse(lp)
+                if (localData) localResponse = { status: 'success', data: localData }
+                if (localData && error) localResponse = { status: 'error', error: 'Local error' }
+            } catch (error) { }
+            return localResponse
+        },
         async fetch(error = false) {
             this.status = 'loading'
             await new Promise(f => setTimeout(f, 1500));
 
             const query: todoQuery = {}
             if (error) query.error = 'test error'
-            const projects = await $fetch('/api/projects', { query })
 
-            if (projects.status === 'success') {
+            let localProjects = this.local('projects', error)
+            const projects = localProjects || await $fetch('/api/projects', { query })
+
+            if (projects?.status === 'success') {
                 this.projects = projects.data
                 this.status = 'success'
                 this.error = null
-            } else if (projects.status === 'error') {
+            } else if (projects?.status === 'error') {
                 this.projects = []
                 this.status = 'error'
-                this.error = projects.error
+                this.error = projects.error || 'ERR'
             }
 
-            const todos = await $fetch('/api/todos')
-            if (todos.status === 'success') {
-                this.todos = todos.data
+            let localTodos = this.local('todos')
+            const todos = localTodos || await $fetch('/api/todos')
+
+            if (todos?.status === 'success') {
+                this.todos = todos?.data
 
                 this.projects = this.projects.map(el => {
                     const projectTodos = this.todos.filter(item => item.project_id === el.id)
@@ -59,6 +79,9 @@ export const useTodoStore = defineStore('todoStore', {
                     return el
                 })
             }
+
+            localStorage.setItem('todos', JSON.stringify(this.todos))
+            localStorage.setItem('projects', JSON.stringify(this.projects))
         }
     }
 })
